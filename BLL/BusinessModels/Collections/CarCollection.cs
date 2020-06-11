09be8a4c7e253ee46e7034d87000efa48cea3e08
Vlad <gometal@ukr.net>
2020-06-11 12:11:33 +0300

    Add iterator pattern

---
+++ BLL/BusinessModels/Collections/CarCollection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BLL.BusinessModels.Models.Sensor;
+using DAL.BusinessModels.Models.Sensor;
+
+namespace BLL.BusinessModels.Collections
+{
+    public class CarCollection : IteratorAggregate
+    {
+        List<ICar> _collection = new List<ICar>();
+
+        bool _direction = false;
+
+        public void ReverseDirection()
+        {
+            _direction = !_direction;
+        }
+
+        public List<ICar> getItems()
+        {
+            return _collection;
+        }
+
+        public void AddItem(ICar item)
+        {
+            this._collection.Add(item);
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return new CarIterator(this, _direction);
+        }
+    }
+}
---
+++ BLL/BusinessModels/Collections/IteratorAggregate.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+namespace BLL.BusinessModels.Collections
+{
+    public abstract class IteratorAggregate : IEnumerable
+    {
+        public abstract IEnumerator GetEnumerator();
+    }
+}
