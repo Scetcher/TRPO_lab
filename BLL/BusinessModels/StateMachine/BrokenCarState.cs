b504acf098f6fc68e8f41478b6843d43528a1330
Vlad <gometal@ukr.net>
2020-06-11 12:15:31 +0300

    Add state pattern

---
+++ BLL/BusinessModels/StateMachine/BrokenCarState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BLL.BusinessModels.Models.Sensor;
+using DAL.BusinessModels.Models.Sensor;
+
+namespace BLL.BusinessModels.StateMachine
+{
+    public class BrokenCarState : ICarState
+    {
+        public void Move(ICar car, string name)
+        {
+            Console.WriteLine($"{name} broken");
+        }
+    }
+}
